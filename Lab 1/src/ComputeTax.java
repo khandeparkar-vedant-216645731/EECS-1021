26c0360a0a8080b9b3cf7611cecdd41e2219ab10
Vedant Khandeparkar <vedantk1@my.yorku.ca>
2019-01-19 19:43:57 -0500

    Added Graded Tasks

---
+++ Lab 1/src/ComputeTax.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class ComputeTax {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter your name:");
+		String name = input.nextLine();
+		
+		System.out.println(name + ", enter your filing status (1 - Single Filing; 2 - Married Filing)");
+		int status = input.nextInt();
+		input.nextLine();
+		
+		if (status < 1 || status > 2) {
+			System.out.println("Illegal status: " + status);
+		}
+		else if (status == 1) {
+			System.out.println(name + ", enter your taxable income:");
+			double income = input.nextDouble();
+			input.nextLine();
+			if (income > 0 && income <= 8350) {
+				double payment1 = (0.1)*(income);
+				System.out.println(name + ", you should pay $" + payment1);
+			}
+			else if (income > 8350 && income <= 33950) {
+				double part1 = (0.1)*8350;
+				double part2 = (0.15)*(income-8350);
+				double payment2 = part1 + part2;
+				System.out.println(name + ", you should pay $" + payment2);
+				System.out.println("Calculation is based on the scheme of Single Filing:");
+				System.out.println("Part I: " + part1);
+				System.out.println("Part II: " + part2);
+			}
+			else {
+				double part1 = (0.1)*8350;
+				double part2 = (0.15)*(33950-8350);
+				double part3 = (0.25)*(income-(33950));
+				double payment3 = part1 + part2 + part3;
+				System.out.println(name + ", you should pay $" + payment3);
+				System.out.println("Calculation is based on the scheme of Single Filing:");
+				System.out.println("Part I: " + part1);
+				System.out.println("Part II: " + part2);
+				System.out.println("Part II: " + part3);
+			}
+		}
+		else {
+			System.out.println(name + ", enter your taxable income:");
+			double income = input.nextDouble();
+			input.nextLine();
+			if (income > 0 && income <= 16700) {
+				double payment4 = (0.1)*income;
+				System.out.println(name + ", you should pay $" + payment4);
+			}
+			else if (income > 16700 && income <= 67900) {
+				double part1 = (0.1)*16700;
+				double part2 = (0.15)*(income-16700);
+				double payment5 = part1 + part2;
+				System.out.println(name + ", you should pay $" + payment5);
+				System.out.println("Calculation is based on the scheme of Married Filing:");
+				System.out.println("Part I: " + part1);
+				System.out.println("Part II: " + part2);
+			}
+			else {
+				double part1 = (0.1)*16700;
+				double part2 = (0.15)*(67900-16700);
+				double part3 = (0.25)*(income-(67900));
+				double payment6 = part1 + part2 + part3;
+				System.out.println(name + ", you should pay $" + payment6);
+				System.out.println("Calculation is based on the scheme of Married Filing:");
+				System.out.println("Part I: " + part1);
+				System.out.println("Part II: " + part2);
+				System.out.println("Part II: " + part3);
+		}
+	}
+	input.close();
+}
+}
---
+++ Lab 1/src/RockPaperScissorsGame.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class RockPaperScissorsGame {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int winsP1 = 0;
+		int winsP2 = 0;
+		
+		System.out.println("Enter the name of player 1:");
+		String player1 = input.nextLine();
+		
+		System.out.println("Enter the name of player 2:");
+		String player2 = input.nextLine();
+		
+		System.out.println("========");
+		System.out.println("Round 1");
+		System.out.println("========");
+		
+		System.out.println("What does " + player1 + " play? (R, P or S)");
+		String R1P1 = input.nextLine();
+		
+		System.out.println("What does " + player2 + " play? (R, P or S)");
+		String R1P2 = input.nextLine();
+		
+		if (R1P1.equals("R") && R1P2.equals("S") || (R1P1.equals("S") && R1P2.equals("P")) || (R1P1.equals("P") && R1P2.equals("R"))) {
+			winsP1 = winsP1 + 1;
+		}
+		else if (R1P2.equals("R") && R1P1.equals("S") || (R1P2.equals("S") && R1P1.equals("P")) || (R1P2.equals("P") && R1P1.equals("R"))) {
+			winsP2 = winsP2 + 1;
+		}
+		else 
+			winsP1 = winsP1 + 0;
+		    winsP2 = winsP2 + 0;
+		
+		System.out.println("========");
+		System.out.println("Round 2");
+		System.out.println("========");
+		
+		System.out.println("What does " + player1 + " play? (R, P or S)");
+		String R2P1 = input.nextLine();
+		
+		System.out.println("What does " + player2 + " play? (R, P or S)");
+		String R2P2 = input.nextLine();
+		
+		if (R2P1.equals("R") && R2P2.equals("S") || (R2P1.equals("S") && R2P2.equals("P")) || (R2P1.equals("P") && R2P2.equals("R"))) {
+			winsP1 = winsP1 + 1;
+		}
+		else if (R2P2.equals("R") && R2P1.equals("S") || (R2P2.equals("S") && R2P1.equals("P")) || (R2P2.equals("P") && R2P1.equals("R"))) {
+			winsP2 = winsP2 + 1;
+		}
+		else {
+			winsP1 = winsP1 + 0;
+		    winsP2 = winsP2 + 0;
+		}
+		
+		if ((winsP1 > winsP2) || (winsP2 > winsP1)) {
+			if (winsP1 > winsP2) {
+				System.out.println("Game over: " + player1 + " wins!");
+			}
+			else {
+				System.out.println("Game over: " + player2 + " wins!");
+			}
+		}
+		else {
+			
+			System.out.println("========");
+			System.out.println("Round 3");
+			System.out.println("========");
+			
+			System.out.println("What does " + player1 + " play? (R, P or S)");
+			String R3P1 = input.nextLine();
+			
+			System.out.println("What does " + player2 + " play? (R, P or S)");
+			String R3P2 = input.nextLine();
+			
+			if (R3P1.equals("R") && R3P2.equals("S") || (R3P1.equals("S") && R3P2.equals("P")) || (R3P1.equals("P") && R3P2.equals("R"))) {
+				winsP1 = winsP1 + 1;
+			}
+			else if (R3P2.equals("R") && R3P1.equals("S") || (R3P2.equals("S") && R3P1.equals("P")) || (R3P2.equals("P") && R3P1.equals("R"))) {
+				winsP2 = winsP2 + 1;
+			}
+			else {
+				winsP1 = winsP1 + 0;
+			    winsP2 = winsP2 + 0;
+		}	
+			
+		   if ((winsP1 > winsP2) || (winsP2 > winsP1)) {
+			  if (winsP1 > winsP2) {
+				 System.out.println("Game over: " + player1 + " wins!");
+			  }
+			  else {
+				  System.out.println("Game over: " + player2 + " wins!");
+			   }
+			 }
+		   else {
+			   System.out.println("Game over: a tie between " + player1 + " and " + player2);
+		  }
+		}	
+    input.close();
+	}
+}
