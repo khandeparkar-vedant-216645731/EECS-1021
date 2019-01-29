c7a183ebd84d1f43037d15906664a5efce537c8d
Vedant Khandeparkar <vedantk1@my.yorku.ca>
2019-01-29 12:19:42 -0500

    Examples Developed by Studying Tutorials Videos

---
+++ Lab2/src/DebuggerIfStatement.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DebuggerIfStatement {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter an integer:");
+		int x = input.nextInt();
+		
+		if (x < 0) {
+			System.out.println("x is negative");
+		}
+		else { // x > 0
+			if (5<=x && x<=10) {
+				System.out.println("x is between 5 and 10");
+			}
+			else if (15 <= x && x <= 20) {
+				System.out.println("x is between 15 and 20");
+			}
+			else { // x < 5 || x > 10
+				System.out.println("x is not in [5,10] and not in [15,20]");
+			}
+		}
+		input.close();
+	}
+}
