dd91f49f6bf358a1da9ad05cce7b6f6012944df8
Vedant Khandeparkar <vedantk1@my.yorku.ca>
2019-02-25 18:52:35 -0500

    Added Graded Tasks

---
+++ Lab 4/src/DetermineFibSeq.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class DetermineFibSeq {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int r = 1;
+		
+		while (r == 1) {
+			int[] seq = new int[20];
+			int count = 0;
+			
+			for (int i = 0; i < 20; i++) {
+				System.out.println("Enter a positive integer value, or -1 to stop:");
+				int a = input.nextInt();
+				input.nextLine();
+				
+				if (a == -1) {
+					break;
+				}
+				else {
+					seq[i] = a;
+			    }
+		    }
+			
+			for (int i = 0; i < seq.length; i++) {
+				if (seq[i] != 0) {
+					count = count + 1;
+				}
+				else {
+					break;
+				}
+			}
+			
+			int[] isSeq = new int[count];
+			
+			System.out.print("The sequence you entered <");
+			
+			for (int i = 0; i < count; i++) {
+				isSeq[i] = seq[i];
+				if (i == count-1) {
+					System.out.println(isSeq[i] + ">");
+				}
+				else {
+					System.out.print(isSeq[i] + ", ");
+				}
+			
+			}
+			
+			int x = 1;
+			int p = 0;
+			
+			while (x > 0) {
+				if (isSeq[0] != 1 || isSeq[1] != 1) {
+					p = 0;
+				}
+				else {
+					for (int i = 2; i <= count-2; i++) {
+						if (isSeq[i] == isSeq[i-1] + isSeq[i-2]) {
+							p = 1;
+						}
+						else {
+							p = 0;
+						}
+					}
+				}
+				x = -1;
+			}
+			
+			if (p == 1) {
+				System.out.println("is the first " + count + " numbers in the Fibonacci Sequence.");
+			}
+			else {
+				System.out.println("is not the first " + count + " numbers in the Fibonacci Sequence.");
+			}
+			
+			while (x < 0) {
+				System.out.println("Would you like to try another sequence? (Y/N)");
+				String ans = input.nextLine();
+				if (ans.equals("Y")) {
+					r = 1;
+					x = 0;
+				}
+				else {
+					System.out.println("Bye!");
+					x = 0;
+					r = -1;
+				}
+			}
+		}
+	    input.close();
+    }
+}
---
+++ Lab 4/src/GenerateFibSeq.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class GenerateFibSeq {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int x = 1;
+		
+		while (x > 0) {
+			System.out.println("Enter the size of Fibonacci Sequence:");	
+			int size = input.nextInt();
+			input.nextLine();
+			if (size <= 0) {
+				System.out.println("Error: Fibonacci Sequence must have size >= 1");
+				x = -1;
+			} 
+			else {
+				int p1 = 1;
+				int p2 = 0;
+				int y = 0;
+				System.out.println("Fibonacci Sequence of size " + size + ":");
+				System.out.print("<1, ");
+				for (int i = 0; i < size - 1; i++) {
+					if (i == size - 2) {
+						y = p1 + p2;
+						System.out.println(y + ">");
+						x = -1;
+					}
+					else {
+						y = p1 + p2;
+						System.out.print(y + ", ");
+					}
+					p2 = p1;
+					p1 = y;
+				}
+				
+				System.out.println("Would you like to continue? (Y/N)");
+				String ans = input.nextLine();
+				if (ans.equals("N")) {
+					System.out.println("Bye!");
+					x = 0;
+				}
+				else if (ans.equals("Y")) {
+					x = 1;
+			    }
+		    }
+		}
+        input.close();
+	}
+}
