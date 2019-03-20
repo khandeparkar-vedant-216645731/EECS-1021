7e64ae2c4632b3c5c83e7388a78021c849596660
Vedant Khandeparkar <vedantk1@my.yorku.ca>
2019-03-20 14:21:56 -0400

    Examples Developed by Studying Tutorials Videos for Lab 6

---
+++ Lab 6/src/model/Student.java
@@ -0,0 +1,128 @@
+package model;
+
+// Each student may be registered in up to 5 courses.
+
+public class Student {
+	public String name;
+	public final int MAX_NUM_COURSES = 5;
+	public CourseRecord[] courses; // each element in the array stores the address of some CourseRecord object
+	// 1. value  of noc represents the number of CourseRecord objects stored in the courses array.
+	// 2. value of noc denotes where in the array 'courses' is the next CourseRecord object.
+	public int noc; // number of courses
+	
+	public Student (String name) {
+		this.name = name;
+		this.courses = new CourseRecord[MAX_NUM_COURSES];
+		this.noc = 0; // so far 0 courses registered; the next course is to be stored at index 0.
+	}
+	
+	// Version 2: Given a CourseRecord object to be stored directly into courses array.
+	public void addCourse(CourseRecord c) {
+		this.courses[this.noc]= c; 
+		this.noc ++;
+	}
+	
+	// Version 2: Given title which is sufficient to create a new CourseRecord object.
+	public void addCourse(String title) {
+		CourseRecord c = new CourseRecord(title);
+		this.addCourse(c);
+//		this.courses[this.noc] = c; 
+//		this.noc ++;
+	}
+	
+	// Given the title of course, return the mark for that course.
+	// If the course does not exist, return -1
+	public int getMarks(String title) {
+		int marks = -1;
+		int index = this.indexOf(title);
+		if (index >= 0) {
+//			CourseRecord c = this.courses[index];
+//			marks = c.getMarks();
+			marks = this.courses[index].getMarks();
+		}
+		return marks;
+	}
+	
+	// Given the title of the course, set the mark of that course.
+	// If the course does not exist, do nothing
+	public void setMarks(String title, int marks) {
+		int index = this.indexOf(title);
+		if (index >= 0) {
+//			CourseRecord c = this.courses[index];
+//			c.setMarks(marks);
+			this.courses[index].setMarks(marks);
+		}
+	}
+	
+	// Helper method reused by getMarks and SetMarks
+	// Given the title of course, return the index of the corresponding course objects
+	// If the title does not exist, return -1 as the index
+	int indexOf(String title) {
+		int index = -1;
+		boolean found = false;
+		for (int i = 0; i < this.noc && !found; i++) {
+			if (this.courses[i].getTitle().equals(title)) {
+				found = true;
+				index = i;
+			}
+		}
+		return index;
+	}
+	
+	double getGPA() {
+		double gpa = 0.0;
+		double gp = 0.0;
+		for (int i = 0; i < this.noc; i ++) {
+//			CourseRecord c = this.courses[i];
+//			String lg = c.getLetterGrade();
+			String lg = this.courses[i].getLetterGrade();
+			if (lg.equals("A+")) {
+				gp += 9;
+			}
+			else if (lg.equals("A")) {
+				gp += 8;
+			}
+			else if (lg.equals("B")) {
+				gp += 7;
+			}
+			else if (lg.equals("C")) {
+				gp += 6;
+			}
+			else if (lg.equals("D")) {
+				gp += 5;
+			}
+			else if (lg.equals("F")) {
+				gp += 0;
+			}
+		}
+		gpa = gp/this.noc;
+		return gpa;
+	}
+
+	
+	public String getDescription() {
+		String result = "";
+		
+		// Version 1: Print out the addresses of courses array elements (which might be null)
+//		result += "Student " + this.name + " has registered " + this.noc + " courses:\n";
+//		for (int i = 0; i < this.courses.length; i++) {
+//			result += this.courses[i] + "\n";
+//		}
+		
+		// Version 2: Print out the description of courses array elements (by calling getDescription from CourseRecord)
+		// This version may cause NullPointerException, because there might be slots of the courses array storing null 
+//		result += "Student " + this.name + " has registered " + this.noc + " courses:\n";
+//		for (int i = 0; i < this.courses.length; i++) {
+//			result += this.courses[i].getDescription() + "\n";
+//		}
+		
+		// Version 3
+		result += "Student " + this.name + " has registered " + this.noc + " courses (with GPA " + this.getGPA() + "):\n";
+		for (int i = 0; i < this.noc; i++) {
+			result += this.courses[i].getDescription() + "\n";
+		}
+		
+		return result;
+	}
+
+}
